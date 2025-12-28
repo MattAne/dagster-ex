150fc2ee76874f651c0ab895598d989f7e5955e3
MattAne <anemyrmattias@gmail.com>
2025-12-28 07:33:15 +0100

    cbg

--- dagster/src/dagster/definitions.py
+++ dagster/src/dagster/definitions.py
@@ -5,4 +5,5 @@ from dagster import definitions, load_from_defs_folder
 
 @definitions
 def defs():
+    print(path_within_project=Path(__file__).parent)
     return load_from_defs_folder(path_within_project=Path(__file__).parent)
