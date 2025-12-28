d13590a3d703edc709c2e4b21c058e90dd00686f
MattAne <anemyrmattias@gmail.com>
2025-12-28 07:38:38 +0100

    cng

--- dagster/src/dagster/definitions.py
+++ dagster/src/dagster/definitions.py
@@ -6,4 +6,6 @@ from dagster import definitions, load_from_defs_folder
 @definitions
 def defs():
     print(path_within_project=Path(__file__).parent)
-    return load_from_defs_folder(path_within_project=Path(__file__).parent)
+    #return load_from_defs_folder(path_within_project=Path(__file__).parent)
+    return load_from_defs_folder(path_within_project="/opt/app/dagster/src/dagster/defs")
+    
